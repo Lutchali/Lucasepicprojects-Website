951556dd6452587ae8000958c51afba5a7526175
Lutchali <luca@vb-schuster.de>
2020-09-06 17:36:35 +0200

    added images to slideshow

--- js-files/dooing.js
+++ js-files/dooing.js
@@ -1,62 +1,28 @@
-console.log("Hello World");
-/*Shows in the console the user choice*/
-var userChoice = 0;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-    setup();
-});
-
-//looks wich Buton is pressed
-function setup() { 
-    $('#home').click(function() {
-        checkResult(00);
-    })
-    $('#about').click(function() {
-        checkResult(01);
-    })
-    $('#ourTeam').click(function() {
-        checkResult(02);
-    })
-    $('#campSites').click(function() {
-        checkResult(03);
-    })
-    $('#missionAndVision').click(function() {
-        checkResult(04);
-    })
-    $('#recources').click(function() {
-        checkResult(05);
-    })
-    $('#tingsYouCanDo').click(function() {
-        checkResult(06);
-    })
-    $('#activities').click(function() {
-        checkResult(07);
-    })
-    $('#parks').click(function() {
-        checkResult(08);
-    })
-    $('#shops').click(function() {
-        checkResult(09);
-    })
-    $('#events').click(function() {
-        checkResult(10);
-    })
-    $('#contact').click(function() {
-        checkResult(11);
-    })
-    $('#map').click(function() {
-        checkResult(12);
-    })
-    $('#directions').click(function() {
-        checkResult(13);
-    })
-    $('#news').click(function() {
-        checkResult(14);
-    })
-   
-    checkResult();
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-function checkResult(userChoice) {
-    console.log(userChoice)
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
